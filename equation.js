e0d4866ad1986972b4a21a7ae3c6f2296c25ecba
holdennekt <ngryshchak@gmail.com>
2022-02-22 01:45:21 +0200

    make variables even more understandable

--- equation.js
+++ equation.js
@@ -3,16 +3,16 @@
 const fs = require("fs");
 const readline = require("readline");
 const fileName = process.argv[2];
-const fileTemplate = /^-?\d+\.?\d*\s-?\d+\.?\d*\s-?\d+\.?\d*\n$/;
+const fileTemplateNumber = /^-?\d+\.?\d*\s-?\d+\.?\d*\s-?\d+\.?\d*\n$/;
 const numberTemplate = /^-?\d+\.?\d*$/;
-const question = (rl, query) =>
+const questionToUser = (rl, query) =>
   new Promise((resolve) => {
     rl.question(query, (answer) => {
       resolve(answer);
     });
   });
 
-const findRoots = (a, b, c) => {
+const findRootsOfQuadraticEquation = (a, b, c) => {
   const d = b * b - 4 * a * c;
   if (d < 0) return [];
   if (d === 0) {
@@ -25,7 +25,7 @@ const findRoots = (a, b, c) => {
   }
 };
 
-const solveEquation = (a, b, c) => {
+const solveQuadraticEquation = (a, b, c) => {
   if (a === 0) throw new Error("a cannot be 0");
   const fixed = [a, b, c].map((val) => val.toFixed(1));
   const eq = `(${fixed[0]}) x^2 + (${fixed[1]}) x + (${fixed[2]}) = 0`;
@@ -41,7 +41,7 @@ const readNumFromRl = async (rl, strNum) => {
   let n;
   do {
     if (n) console.log(`Expected a valid real number, got "${n}" instead`);
-    n = await question(rl, `${strNum} = `);
+    n = await questionToUser(rl, `${strNum} = `);
   } while (!numberTemplate.test(n));
   return parseFloat(n);
 };
