c9e0422ffcca21eca1fdd23cd21e20cd4033e050
holdennekt <ngryshchak@gmail.com>
2022-02-21 23:19:34 +0200

    implement function solve() to find roots

---
+++ equation.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const solve = (a, b, c) => {
+  const d = b * b - 4 * a * c;
+  if (d < 0) return [];
+  if (d === 0) {
+    const x = -1 * b / (2 * a);
+    return [x];
+  } else {
+    const x1 = (-1 * b + Math.sqrt(d)) / (2 * a);
+    const x2 = (-1 * b - Math.sqrt(d)) / (2 * a);
+    return [x1, x2];
+  }
+};
+
